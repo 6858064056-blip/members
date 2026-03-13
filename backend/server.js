28853dda7d04293f9d4820680c57551d68bc0666
6858064056-blip <6858064056@student.chula.ac.th>
2026-03-13 11:23:09 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+let counter = 0;
+
+app.post("/increment_counter", (req, res) => {
+  counter++;
+  res.json({ counter: counter });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
